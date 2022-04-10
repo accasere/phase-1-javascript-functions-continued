7b6dc3649cd3520f0133cbcc2a78b5820378f256
Antony Caseres <accasere@outlook.com>
2022-04-10 01:14:03 -0500

    Completed Assigment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = (plans= 'go to the office') =>{
+    return (`This Monday, I will ${plans}.`)
+}
+mondayWork('work from home')
+
+const wrapAdjective = (praise = "*") => {
+    return function (morePraise = "special") {
+    // return encouragingPrompt = (morePraise = `special`) => {
+        return `You are ${praise}${morePraise}${praise}!`
+    }
+    
+}
+wrapAdjective()
